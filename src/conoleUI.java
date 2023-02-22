5aa6a2b9c0b49e3d9111f848d979893fa4133de6
william <williamrobot1@gmail.com>
2023-02-21 17:41:56 -0700

    Expanded Ui for mode selection

--- src/conoleUI.java
+++ src/conoleUI.java
@@ -1,5 +1,6 @@
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 public class conoleUI {
     InputStream in;
@@ -17,6 +18,28 @@ public class conoleUI {
     }
     public void intro(){
         out.println("Welcome to my Omok game");
+        gameselect();
     }
+    private void gameselect() {
+        out.println("Please chose a game type you would like to play");
+        out.println("1: Vs. the Ai");
+        out.println("2: Vs. a friend");
+        Scanner k = new Scanner(in);
+        String g = k.next();
+        if (g.equals("1")){
+            out.println("ok");
+        }
+        if (g.equals("2")){
+            out.println("wow");
+        }
+        else{
+            out.println("not a valid choice");
+            gameselect();
+        }
+        k.close();
+        
+        
+    }
+    
     
 }
