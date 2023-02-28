6495f3cf78c4cfca96ea952f96119fba4584f4f0
William D <williamrobot1@gmail.com>
2023-02-27 23:02:19 -0700

    moved to make a package

---
+++ src/game/App.java
@@ -0,0 +1,9 @@
+package game;
+public class App {
+    /** starts the show
+     */
+    public static void main(String[] args) throws Exception {
+        new Controll().run();
+
+    }
+}
---
+++ src/game/hplayer.java
@@ -0,0 +1,9 @@
+package game;
+/** commands for human player physically presant*/
+public class hplayer extends player {
+
+    public hplayer (board field, String token){
+        super(field, token);
+    }
+    
+}
