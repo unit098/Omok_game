992d62b12796a33b345c2f202b83bac1db093871
William D <williamrobot1@gmail.com>
2023-02-27 23:21:52 -0700

    dsaasa

--- src/game/App.java
+++
@@ -1,9 +0,0 @@
-package game;
-public class App {
-    /** starts the show
-     */
-    public static void main(String[] args) throws Exception {
-        new Controll().run();
-
-    }
-}
--- src/game/hplayer.java
+++
@@ -1,9 +0,0 @@
-package game;
-/** commands for human player physically presant*/
-public class hplayer extends player {
-
-    public hplayer (board field, String token){
-        super(field, token);
-    }
-    
-}
