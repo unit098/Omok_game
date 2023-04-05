59f655733bdbed69f5f31b436d1694a3473ee4d7
william <williamrobot1@gmail.com>
2023-04-05 16:56:11 -0600

    Expanded GUI most elements present on window

--- src/game/GUI/Omokui.java
+++ src/game/GUI/Omokui.java
@@ -6,13 +6,17 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
-import javax.swing.JTextField;
+import javax.swing.*;
+import java.awt.event.*;
 
 import game.board;
 
 import java.awt.*;
 
 public class Omokui{
+    JMenuBar menubar = new JMenuBar();
+    JMenu settings_bar = new JMenu("Settings");
+    JMenu gamemode = new JMenu("Gamemode");
     public Omokui(){
     var panel = new JFrame("monkey");
     panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,12 +27,26 @@ public class Omokui{
     var panel5 = new JPanel();
     var panel6 = new JPanel();
     var butg = new ButtonGroup();
+    var butg2 = new ButtonGroup();
     var b1 = new JRadioButton("Human");
     var b2 = new JRadioButton("Machine");
     var b3 = new JButton("Start");
     var l3 = new JLabel(" ");
-
-    panel.setMinimumSize(new Dimension(500, 500));
+    var mb1 = new JRadioButton("Human");
+    var mb2 = new JRadioButton("Machine");
+    var menitem = new JMenuItem("Play");
+    var menitem2 = new JMenuItem("Play");
+    menitem2.setMnemonic('P');
+    menitem2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, InputEvent.CTRL_DOWN_MASK));
+    JTool
+    
+    gamemode.add(mb1);
+    gamemode.add(mb2);
+    settings_bar.add(gamemode);
+    settings_bar.add(menitem2);
+    menubar.add(settings_bar);
+    menubar.add(menitem);
+    panel.setMinimumSize(new Dimension(1000, 1000));
     panell.setLayout(new BorderLayout());
     panel2.setLayout(new BorderLayout());
     panel6.setLayout(new BorderLayout());
@@ -36,12 +54,16 @@ public class Omokui{
     panel3.setLayout(new FlowLayout());
     butg.add(b1);
     butg.add(b2);
+    butg2.add(mb1);
+    butg2.add(mb2);
     panel3.add(new JLabel("select mode: "));
     panel3.add(b1);
     panel3.add(b2);
     panel4.add(b3);
     panel5.add(l3);
-    panel6.add(new Bordwidg(new board(15)));
+    board board = new board(30);
+    board.getboard()[4][4] = "X";
+    panel6.add(new Bordwidg(board));
     panel6.add(panel5, BorderLayout.NORTH);
     panel2.add(panel3, BorderLayout.NORTH);
     panel2.add(panel4);
@@ -49,15 +71,59 @@ public class Omokui{
     panell.add(panel6);
     panel.setContentPane(panell);
     panel.pack();
+    panel.setJMenuBar(menubar);
+    ImageIcon oicon = new ImageIcon("funnymomemnydfsjhfdjasdjl;.png");
+    Image image = oicon.getImage();
+    Image outimage = image.getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH);
+    Icon icon = new ImageIcon(outimage);
+    menitem.setIcon(icon);
+    menitem2.setIcon(icon);
+    menitem.setToolTipText("Uhh it plays? bozo");
     panel.setVisible(true);
 
 
-    b3.addActionListener(e -> {if (b1.isSelected()){l3.setText("Vs. Human selected");}else if (b2.isSelected()){l3.setText("Vs. Machine selected");} else{l3.setText("No mode selected");}});
+    ActionListener playact = new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // TODO Auto-generated method stub
+            if (b1.isSelected()){l3.setText("Vs. Human selected");}else if (b2.isSelected()){l3.setText("Vs. Machine selected");} else{l3.setText("No mode selected");}
+        }
+        
+    };
+
+    ActionListener b1act = new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // TODO Auto-generated method stub
+            b1.setSelected(true);
+            mb1.setSelected(true);
+        }
+        
+    };
+    ActionListener b2act = new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // TODO Auto-generated method stub
+            b2.setSelected(true);
+            mb2.setSelected(true);
+        }
+        
+    };
+    b1.addActionListener(b1act);
+    mb1.addActionListener(b1act);
+    b2.addActionListener(b2act);
+    mb2.addActionListener(b2act);
+    b3.addActionListener(playact);
+    menitem.addActionListener(playact);
+    menitem2.addActionListener(playact);
 
         
     }
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> new Omokui());
+       new Omokui();
 
     }
 }
